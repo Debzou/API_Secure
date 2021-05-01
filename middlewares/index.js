e19c1ff23bef26eef9b0e635915eb48460561c15
debzou <david.petit36@gmail.com>
2021-05-01 17:17:39 +0200

    add regex fro username

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const Models = require('../models');
 
 // Check if the user has a good token
 const authenticateToken = (req, res, next) => {
@@ -14,24 +15,33 @@ const authenticateToken = (req, res, next) => {
     })
 }
 
-// Check if username exists
+// Check if username exists and check regex
 const isExistingUser = (req, res, next) => {
-    const Models = require('../models');
-    Models.Account.find({username : req.body.username.toLowerCase()}, (err, user) => {
-        // check error
-        if (err) throw err;
-        
-        if(user.length !== 0){
-            res.send('ERROR: Username is already taken');
-        }else{
-            next();
-        }
-    });
+   
+    // check regex
+    if(/^[a-zA-Z0-9]+$/.test(req.body.username.toLowerCase())){  
+
+        //Check if username exists
+        Models.Account.find({username : req.body.username.toLowerCase()}, (err, user) => {
+            // check error
+            if (err) throw err;
+            
+            if(user.length !== 0){
+                res.send('ERROR: Username is already taken');
+            }else{
+                next();
+            }
+        });
+
+    }else{
+        res.send('ERROR: Invalid regex for username');
+    }
+    
 }
 
 // Check if email exists
 const isExistingEmail = (req, res, next) => {
-    const Models = require('../models');
+
     Models.Account.find({email : req.body.email}, (err, user) =>{
         // check error
         if (err) throw err;
