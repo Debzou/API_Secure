840b84043872b2f9948a32690d4ac41dabb8d3e1
debzou <david.petit36@gmail.com>
2021-05-01 14:52:26 +0200

    add doc

--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,44 @@ const router = require('express').Router();
 const controller = require('../controllers');
 const middleware = require('../middlewares');
 
-// Sign up
+/** 
+ * Add new user
+ * Check if user and email are already used
+ * @route /signup
+ * @get
+ */
 router.post('/signup', middleware.isExistingUser,  middleware.isExistingEmail,  (req,res)=>{
     controller.signUpPerson(req,res);
 });
 
-// Basic auth http
-// cf --> req.headers.authorization
+/** 
+ * Basic Auth
+ * @route /login
+ * @get
+ */
 router.get('/login',(req,res)=>{
     controller.logInPerson(req,res);
 });
 
-// Log Out
-router.get('/logout-server',(req,res) => {
-    controller.logOut(req, res);
-});
-
-// check if a user is connected
+/** 
+ * Bearer Token
+ * check if the token is validate
+ * @route /proveAnthentication
+ * @get
+ */
 router.get('/proveAnthentication',middleware.authenticateToken,(req,res) => {
     controller.proveAnthentication(req, res);
 });
 
-// Hello world
+/** 
+ * Hellworld
+ * @route /
+ * @get
+ * @res : end("welcome in API")
+ */
 router.get('/',(req,res)=>{
     res.end("welcome in API")
 });
 
-
+// export module
 module.exports=router;
